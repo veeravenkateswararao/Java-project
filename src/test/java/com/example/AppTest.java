ede009d9e940ede3265dfb4454143db19aa5eaca
veeravenkateswararao <veeravenkateswararao05@gmail.com>
2026-09-03 15:15:40 +0530

    modify apptest.java file

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,18 +1,12 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class AppTest {
 
     @Test
-    void greetNullShouldReturnHelloWorld() {
-        assertEquals("Hello, World!", App.greet(null));
-    }
-
-    @Test
-    void greetNameShouldReturnHelloName() {
-        assertEquals("Hello, Alice!", App.greet("Alice"));
+    void contextLoads() {
     }
 }
-
